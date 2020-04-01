9130196395f8e05ae703da9e0f8999c94a0d8b57
cliang0818 <cathy.jy.liang@gmail.com>
2020-04-01 19:39:45 +1100

    latest update

--- app/src/main/java/com/example/homeworkassignment2/MainActivity.java
+++ app/src/main/java/com/example/homeworkassignment2/MainActivity.java
@@ -10,13 +10,13 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-
-    String string1 [], string2[], string3[];
-    int images[] = {R.drawable.mcdonalds, R.drawable.mejico, R.drawable.holybasil, R.drawable.thespiceroom,
-    R.drawable.temasek, R.drawable.devon, R.drawable.jasmin, R.drawable.chochosan, R.drawable.helloauntie, R.drawable.bayvista,
+//4 string arrays and 2 integer arrays
+    String string1 [], string2[], string3[], string4[];
+    int images[] = {R.drawable.chochosan, R.drawable.mejico, R.drawable.holybasil, R.drawable.thespiceroom,
+    R.drawable.temasek, R.drawable.devon, R.drawable.jasmin, R.drawable.mcdonalds, R.drawable.helloauntie, R.drawable.bayvista,
     R.drawable.seabay, R.drawable.miles};
-    int images2[] = {R.drawable.stars, R.drawable.stars, R.drawable.stars, R.drawable.stars, R.drawable.stars, R.drawable.stars, R.drawable.stars,
-            R.drawable.stars, R.drawable.stars, R.drawable.stars, R.drawable.stars, R.drawable.stars};
+    int images2[] = {R.drawable.four, R.drawable.five, R.drawable.one, R.drawable.four, R.drawable.five, R.drawable.three, R.drawable.four,
+            R.drawable.four, R.drawable.five, R.drawable.three, R.drawable.one, R.drawable.three};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +25,18 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recyclerView);
 
+        //string1 used to store string array for restaurant name
+        //string2 used to store string array for type of cuisine
+        //string3 used to store string array for location of restaurant
+        //string4 used to store string array for the cost of restaurant for 2 people
         string1 = getResources().getStringArray(R.array.restaurantname);
         string2 = getResources().getStringArray(R.array.cuisine);
         string3 = getResources().getStringArray(R.array.location);
+        string4 = getResources().getStringArray(R.array.foodcost);
 
-        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(this, string1, string2, string3, images, images2);
+
+
+        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(this, string1, string2, string3, string4, images, images2);
 recyclerView.setAdapter(restaurantAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
--- app/src/main/java/com/example/homeworkassignment2/RestaurantAdapter.java
+++ app/src/main/java/com/example/homeworkassignment2/RestaurantAdapter.java
@@ -1,6 +1,7 @@
 package com.example.homeworkassignment2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,21 +9,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+//create new inner class, RestaurantViewHolder
 public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.RestaurantViewHolder> {
 
-    String data1[], data2[], data3[];
+    String data1[], data2[], data3[], data4[];
     int images[];
     int images2[];
     Context context;
 
 
-    public RestaurantAdapter(Context ct, String string1[], String string2[], String string3[], int photos[], int photos2[]) {
+    public RestaurantAdapter(Context ct, String string1[], String string2[], String string3[], String string4[], int photos[], int photos2[]) {
+        //variables to hold the values which will be passed inside MainActivity
         context = ct;
         data1 = string1;
         data2 = string2;
         data3 = string3;
+        data4 = string4;
         images = photos;
         images2 = photos2;
 
@@ -32,21 +37,40 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Re
     @NonNull
     @Override
     public RestaurantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        //inflate my_row
+
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.my_row, parent, false);
         return new RestaurantViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RestaurantViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RestaurantViewHolder holder, final int position) {
         holder.myText1.setText(data1[position]);
         holder.myText2.setText(data2[position]);
         holder.myText3.setText(data3[position]);
+        holder.myText4.setText(data4[position]);
         holder.myImage.setImageResource(images[position]);
         holder.myImage2.setImageResource(images2[position]);
 
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, SecondActivity.class);
+                intent.putExtra("data1",data1[position]);
+                intent.putExtra("data2",data2[position]);
+                intent.putExtra("data3",data3[position]);
+                intent.putExtra("data4",data4[position]);
+                intent.putExtra("images",images[position]);
+                intent.putExtra("images2",images2[position]);
 
-    }
+
+                context.startActivity(intent);
+
+            }
+    } );
+}
 
 
     @Override
@@ -55,19 +79,24 @@ public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.Re
     }
 
     public class RestaurantViewHolder extends RecyclerView.ViewHolder {
-
-        TextView myText1, myText2, myText3;
+//created textview and imageview objects
+        TextView myText1, myText2, myText3, myText4;
         ImageView myImage, myImage2;
+        ConstraintLayout mainLayout;
 
 
         public RestaurantViewHolder(@NonNull View itemView) {
             super(itemView);
 
+
+//find ID
             myText1 = itemView.findViewById(R.id.restaurant);
             myText2 = itemView.findViewById(R.id.cuisine);
             myText3 = itemView.findViewById(R.id.location);
+            myText4 = itemView.findViewById(R.id.foodcost);
             myImage = itemView.findViewById(R.id.image);
             myImage2 = itemView.findViewById(R.id.rating);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
 
         }
 
